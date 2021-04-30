3f598996a393bae40826888f377748a66ac2a0c2
Jiaying330 <56377531+Jiaying330@users.noreply.github.com>
2021-04-30 09:16:17 -0700

    event class

--- code/schedule.js
+++ code/schedule.js
@@ -1,21 +1,49 @@
+class eventOb {
+	/*
+		text: name of the event
+		date: date of the event
+		repeat: which days do the user want to repeat the event
+		alarm: does the user want to set an alarm
+	*/
+	constructor(text, time, repeat, alarm) {
+		this.text = text;
+		this.time = time;
+		this.repeat = repeat;
+		this.alarm = alarm;
+	}
+	setText(newText) {
+		this.text = newText;
+	}
+	setDate(newTime) {
+		this.time = newTime;
+	}
+	setRepeat(newRepeat) {
+		this.repeat = newRepeat;
+	}
+	setAlarm(newAlarm) {
+		this.alarm = newAlarm;
+	}
+}
+
 var eList = document.getElementById("eventList");
 //load stored events from chrome storage
-/* Fill out the events on start up */
-// const eList = document.getElementById("eventList");
 chrome.storage.local.get(["events"], function(result) {
 	if(result.events == undefined){
-		console.log("here");
 		chrome.storage.local.set({"events": []});
 	} else {
+		// var resultList = result.events;
+		// var copyResultList = [];
+		// for(var i = 0; i < resultList.length; i++){
+		// 	var reader = JSON.parse(resultList[i]);
+		// 	copyResultList.push(JSON.stringify({
+		// 		text: reader.text,
+		// 		time: reader.time,
+		// 	}));
 		var resultList = result.events;
 		var copyResultList = [];
 		for(var i = 0; i < resultList.length; i++){
-			console.log(i);
 			var reader = JSON.parse(resultList[i]);
-			copyResultList.push(JSON.stringify({
-				text: reader.text,
-				time: reader.time,
-			}));
+			copyResultList.push(JSON.stringify(reader));
 		}
 		
 		chrome.storage.local.set({"events": copyResultList}, function() {
@@ -37,10 +65,11 @@ chrome.storage.local.get(["events"], function(result) {
 
 function createEvent(text, date) {
 		var li = document.createElement("li");
-		var t = document.createTextNode(text);
-		var d = document.createTextNode(date);
-		li.appendChild(t);
-		li.appendChild(d);
+		li.textContent = text + " - " + date;
+		// var t = document.createTextNode(text);
+		// var d = document.createTextNode(date);
+		// li.appendChild(t);
+		// li.appendChild(d);
 		li.id = text;
 		var buttonD = document.createElement("button");
 		buttonD.innerHTML = "Delete";
@@ -58,12 +87,8 @@ function createEvent(text, date) {
 document.addEventListener('DOMContentLoaded', function () {
   var buttonAddEvent = document.getElementById("addEvent");
   var buttonEditEvent = document.getElementById("editEvent");
-//   var buttonDeleteEvent = document.getElementById("deleteEvent");
-  var buttonRepeatEvent = document.getElementById("repeatEvent");
   buttonAddEvent.addEventListener("click", clickAddEvent);
   buttonEditEvent.addEventListener("click", clickEditEvent);
-  buttonRepeatEvent.addEventListener("click", clickRepeatEvent);
-//   buttonDeleteEvent.addEventListener("click", clickDeleteEvent);
 });
 
 
@@ -77,26 +102,20 @@ function clickAddEvent(e) {
 	var timeDifference = (new Date(date)).getTime - now.getTime();
 	var text = document.getElementById("event").value;
 	if(date != "" && text != "") {
+		//extract input
+		var checkBox = document.getElementById("repeat");
+		var chks = checkBox.getElementsByTagName("INPUT");
+		var repeat = new Array();
+		for (var i = 0; i < chks.length; i++) {
+			if(chks[i].checked) {
+				repeat.push(chks[i].value);
+			}
+		}
+		var remind = document.getElementById("remindTime").value;
+		var newEvent = new eventOb(text, date, repeat, remind);
 		// store new event to storage
-		addEvents(text, date);
-		
-		// display new event
-		var li = document.createElement("li");
-		var t = document.createTextNode(text);
-		var d = document.createTextNode(date);
-		li.appendChild(t);
-		li.appendChild(d);
-		li.id = text;
-		var buttonD = document.createElement("button");
-		buttonD.innerHTML = "Delete";
-		buttonD.className = "deleteEvent";
-		li.appendChild(buttonD);
-		document.getElementById("eventList").appendChild(li);
-		buttonD.addEventListener('click', function(){
-			removeEvent(text);
-			var div = this.parentElement;
-			div.style.display = "none";
-		});
+		addEvents(newEvent);
+		eList.appendChild(createEvent(text, date));
 	}
 	else {
 		alert("invalid input");
@@ -114,13 +133,30 @@ function clickEditEvent(e) {
 	var timeDifference = (new Date(date)).getTime - now.getTime();
 	var text = document.getElementById("event").value;
 	if(date != "" && text != "") {
-		editEvent(text, date);
+		var checkBox = document.getElementById("repeat");
+		var chks = checkBox.getElementsByTagName("INPUT");
+		var repeat = new Array();
+		for (var i = 0; i < chks.length; i++) {
+			if(chks[i].checked) {
+				repeat.push(chks[i].value);
+			}
+		}
+		var remind = document.getElementById("remindTime").value;
+		var newEvent = new eventOb(text, date, repeat, remind);
+		editEvent(newEvent);
+		
 		var li = document.getElementById(text);
-		li.textContent = text + date;
+		li.textContent = text + " - " + date;
 		var buttonD = document.createElement("button");
 		buttonD.innerHTML = "Delete";
 		buttonD.className = "deleteEvent";
 		li.appendChild(buttonD);
+		buttonD.addEventListener('click', function(){
+			removeEvent(text);
+			var div = this.parentElement;
+			div.style.display = "none";
+		});
+		// eList.appendChild(createEvent(text, date));
 	}
 	else {
 		alert("invalid input");
@@ -128,21 +164,10 @@ function clickEditEvent(e) {
 	}
 }
 
-function clickRepeatEvent(e) {
-
-}
-
-/* 
-	output: returns a list of events stored in local storage
-	functon: gets a list of events stored in local storage
-*/
-function addEvents(text, date, i) {
+function addEvents(newEvent) {
 	chrome.storage.local.get({events: []}, function(result) {
 		var list = result.events;
-		list.push(JSON.stringify({
-			text: text,
-			time: date
-		}));
+		list.push(JSON.stringify(newEvent)); 
 		chrome.storage.local.set({"events": list}, function() {
 			console.log('value set to ' + list);
 		});
@@ -160,10 +185,9 @@ function removeEvent(text) {
 	chrome.storage.local.get({events: []}, function(result) {
 		list = result.events;
 		for(var key in list) {
+			var json = JSON.parse(list[key]);
 			console.log(key + ": " + list[key]);
-			var n = list[key].search("\"" + text +"\"");
-			if(n !== -1) {
-				console.log(n);
+			if(json.text.localeCompare(text) == 0){
 				list.splice(key, 1);
 				break;
 			}
@@ -178,7 +202,22 @@ function removeEvent(text) {
 	input: text(name) of the event, time value of the event
 	function: edit an event with the name text
 */
-function editEvent(text, time) {
-	removeEvent(text);
-	addEvents(text, time);
+function editEvent(eEvent) {
+	var list;
+	var i;
+	chrome.storage.local.get({events: []}, function(result) {
+		list = result.events;
+		for(var key in list) {
+			var json = JSON.parse(list[key]);
+			console.log(key + ": " + list[key]);
+			if(json.text.localeCompare(eEvent.text) == 0){
+				list.splice(key, 1);
+				break;
+			}
+		}
+		list.push(JSON.stringify(eEvent)); 
+		chrome.storage.local.set({"events": list}, function() {
+			console.log("after deleting: " + list);
+		})
+	});
 }
