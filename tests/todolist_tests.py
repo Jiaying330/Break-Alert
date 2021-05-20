07ff01e785d9245b6f79d7eed5e5c9a127c57530
trev191 <70498378+trev191@users.noreply.github.com>
2021-05-20 16:26:43 -0700

    created test for adding tasks to todolist (currently only works for my machine only)

--- tests/todolist_tests.py
+++ tests/todolist_tests.py
@@ -6,20 +6,64 @@
 import time
 from selenium import webdriver
 
-path = './chromedriver.exe'  # Add path to the chrome driver
-driver = webdriver.Chrome(path)  # Optional argument, if not specified will search path.
+# link to the chrome extension in .crx form
+extension_path = '../code.crx'
+chop = webdriver.ChromeOptions()
+chop.add_extension(extension_path)
 
+# link to chromedriver.exe
+path_to_chromedriver = './chromedriver.exe'
+driver = webdriver.Chrome(path_to_chromedriver, options=chop)
 
-driver.get('http://www.google.com/')
+# hardcoded id from chromedriver : ifofbjinpigefmlmffefmockecioklam (this opens the popup but the id is unique to my laptop only)
+# id from extension: ikpmenhphlekealoglgagfcednkdcacp
+driver.get('chrome-extension://ifofbjinpigefmlmffefmockecioklam/popup.html')
 
-time.sleep(1) # Let the user actually see something!
+# do nothing for 2 seconds
+time.sleep(2) 
 
-search_box = driver.find_element_by_name('q')
+# switch to todolist tab
+switch_to_todolist_button = driver.find_element_by_name("openTodoListTab")
+switch_to_todolist_button.click()
 
-search_box.send_keys('ChromeDriver')
+# do nothing for 1 seconds
+time.sleep(1)
 
-search_box.submit()
+# select the input box to type data into
+inputTask = driver.find_element_by_id("input")
+inputTask.send_keys("task 1")
 
-time.sleep(1) # Let the user actually see something!
+time.sleep(1)
 
-driver.quit()
+# add task to todo list
+submitTaskButton = driver.find_element_by_id("addTaskListener")
+submitTaskButton.click()
+
+time.sleep(1)
+
+# add a second and third task
+inputTask.send_keys("task 2")
+time.sleep(1)
+submitTaskButton.click()
+time.sleep(1)
+inputTask.send_keys("task 3")
+time.sleep(1)
+submitTaskButton.click()
+time.sleep(2)
+
+# refresh the page to test if the tasks saved
+driver.refresh()
+time.sleep(2)
+
+# navigate back to todolist tab
+switch_to_todolist_button = driver.find_element_by_name("openTodoListTab")
+switch_to_todolist_button.click()
+
+# assert (check) that the todolist size is 3 
+numTasks = driver.find_elements_by_css_selector("div.taskList li")
+# print("todolist:")
+# print(numTasks)
+# print(len(numTasks))
+assert(len(numTasks) == 3)
+
+# driver.quit()
