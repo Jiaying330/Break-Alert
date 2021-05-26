7d229d81d5ce4135e992162ae174e6a3f8b84b2d
trev191 <70498378+trev191@users.noreply.github.com>
2021-05-26 00:24:27 -0700

    have code check if there is an active break alert already before creating a new one (have user disable the current one if so)

--- code/javascript/breakalert.js
+++ code/javascript/breakalert.js
@@ -8,57 +8,100 @@ document.addEventListener('DOMContentLoaded', function () {
 	  buttonDisable.addEventListener("click", disableBreakAlert); 
 });
 
-
 // disable break alert (clear saved data in chrome storage)
 function disableBreakAlert(){
-  alert("break alert disabled!");
+  deleteBreakAlarm();
+  alert("Break alert disabled!");
 }
 
 // enable break alert (save user inputted data into chrome storage
 // and create an alarm)
-function enableBreakAlert(){
+async function enableBreakAlert(){
   var productivePeriod = document.getElementById("productiveDuration").value;
   var breakPeriod = document.getElementById("breakDuration").value;
+  var isActive;
 
+  // add a try and catch block to catch errors of the helper function
+  // (otherwise Chrome throws an error); it seems that when try{} 
+  // executes, isActive is true and vice versa for catch{}
+  try{
+    // force all execution to wait for isActiveBreakAlert() to return
+    isActive = await isActiveBreakAlert();
+    alert("isActive is " + isActive);
+  }
+  catch (status) {
+    // isActive (status) is false
+    alert("isActive is  " + status);
+  }
+  
   // check for empty input boxes before creating the alarm and storing it
   if (productivePeriod == "" || breakPeriod == ""){
     alert("Error: One or more above values are empty!");
   } 
-  
+  else if (isActive){
+    // already an active break alert, so have user disable it first
+    alert("Error: Already an active Break Alert. Please disable it to set a new one");
+  }
   else {
     // call helper function to create/overwrite a new break alert alarm and start
     // in productive mode
-    replaceBreakAlarm(productivePeriod);
+    createBreakAlarm(productivePeriod);
 
     // add the productivePeriod and breakPeriod to chrome storage
     chrome.storage.sync.get("breakAlertData", function(result) {
       var copyBreakAlertData = result.breakAlertData;
 
-      var userInputs = JSON.stringify({'productive' : productivePeriod, 'break' : breakPeriod});
+      // start on productive mode (ie. be productive for 'productivePeriod' mins)
+      var userInputs = JSON.stringify({'productive' : productivePeriod,
+                                       'break' : breakPeriod, 
+                                       'mode' : "productive"});
+
       copyBreakAlertData = userInputs;
 
-      chrome.storage.sync.set({"breakAlertData": copyBreakAlertData}, function(){
+      chrome.storage.sync.set({"breakAlertData": copyBreakAlertData}, function(data){
         alert("after setting breakAlertData: " + copyBreakAlertData);
       });  // end set 
     });  // end get
-
+    
+    alert("Break alert enabled!");
   }  // end else
+
 }
 
 
-// delete the current break alert alarm (if it exists) and create a new
-// alarm with a different duration (switch between productive period and
-// break periods)
+// create a new alarm with a different duration
+// (switch between productive period and break periods)
 //
 // period : productive or break period to set the new alarm at (string)
-function replaceBreakAlarm(period){
-  // delete current break alert alarm if it exists
-  deleteBreakAlarm();
-
-  // create the new break alert alarm (need to convert period from string to int)
-  chrome.alarms.create("BreakAlert", {delayInMinutes : parseInt(period)})
+function createBreakAlarm(period){
+  chrome.alarms.get("BreakAlert", function(alarm) {
+    if (typeof alarm == "undefined"){
+      // create the new break alert alarm (need to convert period from string to int)
+      chrome.alarms.create("BreakAlert", {delayInMinutes : parseInt(period)});
+    }
+  });
 }
 
+// check if there's an existing breakAlert alarm active already and return T/F
+//
+// NOTE: since this function uses chrome.alarms.get which returns a promise, it runs
+//       in the background while other code is executed in the meantime (which means
+//       code dependent on this function may run out of order); to avoid this issue, 
+//       use `await` in a try catch block
+function isActiveBreakAlert(){
+  return new Promise((success, failure) => {
+    chrome.alarms.get("BreakAlert", function(alarm) {
+      if (typeof alarm != "undefined"){
+        // "return" true
+        success(true);
+      }
+      else {
+        // "return" false
+        failure(false);
+      }
+    });
+  })
+}
 
 // delete the current break alert alarm (if it exists)
 function deleteBreakAlarm(){
@@ -66,7 +109,9 @@ function deleteBreakAlarm(){
   chrome.alarms.get("BreakAlert", function(alarm) {
     if (typeof alarm != "undefined"){
       chrome.alarms.clear("BreakAlert");
-      alert("prev alarm cleared");
     }
   });
+
+  // delete the break alert from chrome storage as well
+  chrome.storage.sync.set({"breakAlertData": []});
 }
