65b4e10f063ee21fa1f6cb27c7bdb0e201f12ea3
ShuuShunnju <82403065+ShuuShunnju@users.noreply.github.com>
2021-05-26 13:04:36 -0700

    add clean all logic for website blocker

--- code/blockpage.js
+++ code/blockpage.js
@@ -1,27 +1,41 @@
 const textarea = document.getElementById("textarea");
 const save = document.getElementById("save");
+const clearlink = document.getElementById("clearlink");
 const blockedcheckbox = document.getElementById("blockedcheckbox");
 
-textarea.placeholder = [
-  "After insert link as example, press save and click checkbox to enable the function.",
-  "",
-  "Example:",
-  "facebook.com",
-  "youtube.com",
-  "twitter.com"
-].join("\n");
+if (textarea != null){
+  textarea.placeholder = [
+    "After insert link as example, press save and click checkbox to enable the function.",
+    "",
+    "Example:",
+    "facebook.com",
+    "youtube.com",
+    "twitter.com"
+  ].join("\n");
+}
 
-save.addEventListener("click", () => {
-  const blocked = textarea.value.split("\n").map(s => s.trim()).filter(Boolean);
+if (save != null){
+  save.addEventListener("click", () => {
+    const blocked = textarea.value.split("\n").map(s => s.trim()).filter(Boolean);
+    chrome.storage.local.set({ blocked });
+  });
+}
 
-  chrome.storage.local.set({ blocked });
-});
+if (clearlink != null){
+  clearlink.addEventListener("click", () => {
+    textarea.value = '';
+    const blocked = null;
+    chrome.storage.local.set({blocked});
+  });
+}
 
-blockedcheckbox.addEventListener("change", (event) => {
-  const enabled = event.target.checked;
+if (blockedcheckbox != null){
+  blockedcheckbox.addEventListener("change", (event) => {
+    const enabled = event.target.checked;
 
-  chrome.storage.local.set({ enabled });
-});
+    chrome.storage.local.set({ enabled });
+  });
+}
 
 window.addEventListener("DOMContentLoaded", () => {
   chrome.storage.local.get(["blocked", "enabled"], function (local) {
