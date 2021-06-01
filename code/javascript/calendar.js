597035e69d973f3ad871feb271ad7734f0382ee5
老刁 <446099465@qq.com>
2021-06-01 03:13:52 -0700

    Create event page for calendar UI

--- code/javascript/calendar.js
+++ code/javascript/calendar.js
@@ -52,7 +52,10 @@ function checkEvent(currYear, currMonth, currDay){
   
 // Date object
 const date = new Date();
-  
+
+// Date object for the most recent clicked day element
+var currDateObj = new Date();
+
 /*
 Input: no input
 Output: properly set calendar window
@@ -176,45 +179,73 @@ async function renderCalendar () {
   monthDays.innerHTML = days;
 
   // Add event listener to all the day elements,
-  // Display the scheduler window when click on any of the day elements
-  let schedule = document.querySelectorAll("div.days > div");
-  for (let n = 0; n < schedule.length; n++) {
-    schedule[n].addEventListener("click", () => {
+  // Display the proper calendar event page window when click on any of the day elements
+  let event_page = document.querySelectorAll("div.days > div");
+  for (let n = 0; n < event_page.length; n++) {
+    event_page[n].addEventListener("click", () => {
+
+      // get the date info for the clicked day element
       var inputDate = new Date(date);
-      var inputDay = schedule[n].innerHTML.split('<')[0];
-      inputDate.setDate(inputDay);
-      if (schedule[n].className == "next-date") {
+      var inputDay = event_page[n].innerHTML.split('<')[0];
+
+      if (event_page[n].className == "next-date") {
         var inputMonth = date.getMonth() == 11 ? 0 : date.getMonth() + 1;
         var inputYear = date.getMonth() == 11 ? date.getFullYear() + 1 : date.getFullYear();
         inputDate.setMonth(inputMonth);
         inputDate.setFullYear(inputYear);
-      } else if (schedule[n].className == "prev-date") {
+      } else if (event_page[n].className == "prev-date") {
         var inputMonth = date.getMonth() == 0 ? 11 : date.getMonth() - 1;
         var inputYear = date.getMonth() == 0 ? date.getFullYear() - 1 : date.getFullYear();
         inputDate.setMonth(inputMonth);
         inputDate.setFullYear(inputYear);
       }
+      inputDate.setDate(inputDay);
       inputDate.setMinutes(date.getMinutes() - date.getTimezoneOffset());
 
+      // set the date info for the most recent day element clicked
+      currDateObj = inputDate;
+
       // range checking to make sure inputDate is a valid number
-      if (!isNaN(inputDate)){
+      // set the current date info for event page header and scheduler input
+      if (!isNaN(inputDate)) {
+        document.getElementById("eventPage-header").innerText = inputDate.toISOString().slice(0, 10);
         document.getElementById("eventDate").value = inputDate.toISOString().slice(0, 16);
       }
-      
-      document.querySelector(".scheduler").classList.add("show");
+
+      // get the list of event titles that belongs to the current date element
+      chrome.storage.local.get({events: []}, function (result) {
+        let eventList = result.events;
+        let eventText = "";
+
+        for (let eventIndex in eventList) {
+          let eventDate = eventList[eventIndex].match(/\d{0,6}\-\d{0,2}\-\d{0,2}/g)[0].split("-");
+          let currDate = inputDate.toISOString().match(/\d{0,6}\-\d{0,2}\-\d{0,2}/g)[0].split("-");
+          
+          if (JSON.stringify(eventDate) === JSON.stringify(currDate)) {
+            let textInfo = eventList[eventIndex].match(/\"text\"\:\"(.*?)\"/g)[0].slice(8, -1);
+            eventText += `<li>${textInfo}</li>`;
+          }
+        }
+        if (eventText === "") {
+          document.querySelector(".eventPage-list").innerHTML = "";
+        } else {
+          document.querySelector(".eventPage-list").innerHTML = eventText;
+        }
+      });
+
+      document.querySelector(".calendar-eventPage").classList.add("show");
     });
   }
   
   // Display the hasEvent indicator for the day element when
   // hasEvent = True for this day element
-  for (let m = 0; m < schedule.length; m++) {
-    let event = schedule[m].getAttribute("hasEvent");
+  for (let m = 0; m < event_page.length; m++) {
+    let event = event_page[m].getAttribute("hasEvent");
 
     if (event === "True") {
-      schedule[m].innerHTML += `<i class="fa fa-circle" aria-hidden="true"></i>`;
+      event_page[m].innerHTML += `<i class="fa fa-circle" aria-hidden="true"></i>`;
     } 
   }
-
 };
   
 // Move to previous month calendar
@@ -228,10 +259,10 @@ document.querySelector(".next").addEventListener("click", () => {
   date.setMonth(date.getMonth() + 1);
   renderCalendar();
 });
-  
-// Click to hide the scheduler window
-document.querySelector(".scheduler-return").addEventListener("click", () => {
-  document.querySelector(".scheduler").classList.remove("show");
+
+// Click to hide the calendar event page window
+document.querySelector(".calendar-eventPage-return").addEventListener("click", () => {
+  document.querySelector(".calendar-eventPage").classList.remove("show");
   
   // clear all previous inputs
   var buttonClearInputs = document.getElementById("clearInputs");
@@ -240,6 +271,46 @@ document.querySelector(".scheduler-return").addEventListener("click", () => {
   }
 
   renderCalendar();
+});
+
+// Click to hide the scheduler window
+document.querySelector(".scheduler-return").addEventListener("click", () => {
+  document.querySelector(".scheduler").classList.remove("show");
+
+  // reset the list of event in the event page
+  chrome.storage.local.get({events: []}, function (result) {
+    let eventList = result.events;
+    let eventText = "";
+
+    for (let eventIndex in eventList) {
+      let eventDate = eventList[eventIndex].match(/\d{0,6}\-\d{0,2}\-\d{0,2}/g)[0].split("-");
+      let currDate = currDateObj.toISOString().match(/\d{0,6}\-\d{0,2}\-\d{0,2}/g)[0].split("-");
+
+      if (JSON.stringify(eventDate) === JSON.stringify(currDate)) {
+        let textInfo = eventList[eventIndex].match(/\"text\"\:\"(.*?)\"/g)[0].slice(8, -1);
+        eventText += `<li>${textInfo}</li>`;
+      }
+    }
+
+    if (eventText === "") {
+      document.querySelector(".eventPage-list").innerHTML = "";
+    } else {
+      document.querySelector(".eventPage-list").innerHTML = eventText;
+    }
+  });
+  
+  // clear all previous inputs
+  var buttonClearInputs = document.getElementById("clearInputs");
+  if (buttonClearInputs != null){
+    buttonClearInputs.click();
+  }
+
+  renderCalendar();
+});
+  
+// Click to show the scheduler window
+document.querySelector(".calendar-eventPage-to-scheduler").addEventListener("click", () => {
+  document.querySelector(".scheduler").classList.add("show");
 });
   
 // Display calendar window
